f8900259654b850c0caeadda96e28cb11689c8c9
baoyuansong <47556041+baoyuansong@users.noreply.github.com>
2020-06-17 02:14:59 +0800

    Update product_v2.py

--- product_v2.py
+++ product_v2.py
@@ -14,14 +14,18 @@ dirname, filename = os.path.split(os.path.abspath(sys.argv[0]))
 render = web.template.render(os.path.join(dirname, "templates"))
 class ProductList:
     def GET(self):
-        config = configparser.ConfigParser()
-        config.read(os.path.join(dirname, "conf", "product.conf"))
-        servicename = config.get("stock", "service")
-        serviceport = config.get("stock", "port")
-        conn = httplib.HTTPConnection(servicename + ":" + serviceport, timeout=10)            
-        conn.request("GET", "/stock/list")
-        res = conn.getresponse()
-        content = json.loads(res.read())
+        try:
+            config = configparser.ConfigParser()
+            config.read(os.path.join(dirname, "conf", "product.conf"))
+            servicename = config.get("stock", "service")
+            serviceport = config.get("stock", "port")
+            conn = httplib.HTTPConnection(servicename + ":" + serviceport, timeout=10)            
+            conn.request("GET", "/stock/list")
+            res = conn.getresponse()
+            content = json.loads(res.read())
+        except Exception as e:
+            content = ["fail to get product"]
+        return render.product_list_v2(content)
 
 if __name__ == "__main__":
     app.run()
